a81941b3a45da578e9326639989261aefca0fb35
Mehrshad Baqerzadegan <MehrshadBaqerzadegan@gmail.com>
2023-07-25 21:19:42 +0330

    Fix responses

--- network/views.py
+++ network/views.py
@@ -36,17 +36,19 @@ class Trigger(generics.CreateAPIView, API):
 
                 await asyncio.sleep(5)
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         self.network = serializer.validated_data.get("network", None)
         self.receiver = serializer.validated_data.get("receiver", None)
 
         if Wallet.objects.filter(address=self.receiver).count() == 0:
             response_data = {
                 "status": "error",
-                "message": "Wallet does not exists.",
+                "message": "Wallet does not exist.",
             }
-            response = Response(response_data, status=status.HTTP_403_FORBIDDEN)
-            return response
+            return Response(response_data, status=status.HTTP_403_FORBIDDEN)
 
         additional_data = {"receiver": self.receiver}
         data_to_create = {**serializer.validated_data, **additional_data}
@@ -76,12 +78,55 @@ class Trigger(generics.CreateAPIView, API):
                 "status": "error",
                 "message": "Transaction failed.",
             }
-            response = Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
-        # Add custom header to instruct the client to keep the connection open for 2 minutes
-        # keep_connection_open_time = datetime.now() + timedelta(minutes=2)
-        # response["Keep-Alive"] = f"timeout=120, max=60"
-        # response["Connection"] = "keep-alive"
-        # response["Date"] = http_date(datetime.now().timestamp())
-        # response["Expires"] = http_date(keep_connection_open_time.timestamp())
-        return response
+
+# def perform_create(self, serializer):
+#     self.network = serializer.validated_data.get("network", None)
+#     self.receiver = serializer.validated_data.get("receiver", None)
+
+#     if Wallet.objects.filter(address=self.receiver).count() == 0:
+#         response_data = {
+#             "status": "error",
+#             "message": "Wallet does not exists.",
+#         }
+#         response = Response(response_data, status=status.HTTP_403_FORBIDDEN)
+#         return response
+
+#     additional_data = {"receiver": self.receiver}
+#     data_to_create = {**serializer.validated_data, **additional_data}
+#     transaction = serializer.save(**data_to_create)
+
+#     loop = asyncio.new_event_loop()
+#     asyncio.set_event_loop(loop)
+#     result = loop.run_until_complete(
+#         self.check_txid_in_response(transaction, self.get_api_url())
+#     )
+
+#     if result:
+#         transaction.state = 2  # 2 corresponds to "Done" in STATE_TYPE choices
+#         transaction.save()
+#         print("Transaction completed")
+#         response_data = {
+#             "status": "success",
+#             "message": "Transaction completed successfully.",
+#             "transaction_id": transaction.id,
+#         }
+#         return Response(response_data, status=status.HTTP_200_OK)
+
+#     else:
+#         transaction.state = 3  # 3 corresponds to "Failed" in STATE_TYPE choices
+#         transaction.save()
+#         response_data = {
+#             "status": "error",
+#             "message": "Transaction failed.",
+#         }
+#         response = Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+#     # Add custom header to instruct the client to keep the connection open for 2 minutes
+#     keep_connection_open_time = datetime.now() + timedelta(minutes=2)
+#     response["Keep-Alive"] = f"timeout=120, max=60"
+#     response["Connection"] = "keep-alive"
+#     response["Date"] = http_date(datetime.now().timestamp())
+#     response["Expires"] = http_date(keep_connection_open_time.timestamp())
+#     return response
